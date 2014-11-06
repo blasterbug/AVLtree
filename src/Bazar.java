8900d30ca711646f3e696996f7fad2b630cc9757
blasterbug <tontonposso@gmail.com>
2014-11-06 18:32:32 +0100

    regex ma poule ! :goat:

---
+++ src/Bazar.java
@@ -0,0 +1,33 @@
+/**
+ * Run the current project
+ * @author Benjamin Sientzoff
+ * @version 0.1 9 oct. 2014
+ */
+
+public class Bazar {
+
+	public static void main(String[] args) {
+        String file = "germinal.txt";
+        AVLTree<String> arbre = new AVLTree<String>();
+        TextFileReader germinal = new TextFileReader();
+
+        if(germinal.openTextFile(file)){
+            String words[];
+            //while(germinal.available()){
+                words = germinal.readWordsPerLine();
+                for(String str : words){
+                    System.out.println(str);
+                    arbre.add(str);
+                }
+            //}
+            germinal.closeFile();
+        }
+        else {
+            System.out.println( file + "not found.");
+        }
+
+        System.out.println(arbre.toString());
+
+	}
+
+}
