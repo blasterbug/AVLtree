2cc9e58fd86491dece0525146d23e37ae4450aac
Thomas Minier <tminier01@gmail.com>
2014-12-04 12:58:27 +0000

    maj de ABRNode

--- src/Tree/ABRNode.java
+++ src/Tree/ABRNode.java
@@ -210,11 +210,6 @@ public class ABRNode<T extends Comparable> {
 			i += rightSon.nbCommuns(nodeB);
 		}
 		
-		if(this.isLeaf()) {
-			if(nodeB.contains(tag)) {
-				i++;
-			}
-		}
 		return i;
 	}
 	
